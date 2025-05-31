118d6e41a062d6a232b9d458c380eb0899eea8c6
VickSxnchxz <82923629+VickSxnchxz@users.noreply.github.com>
2025-05-31 11:53:48 -0600

    Update P1.java

--- SPVH-25P/P1/p1/src/paquete/P1.java
+++ SPVH-25P/P1/p1/src/paquete/P1.java
@@ -1,34 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package paquete;
 import java.util.Scanner; 
 
-/**
- *
- * @author Alumno 25
- */
 public class P1 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) { 
- 
-      Scanner scan = new Scanner(System.in); 
+    Scanner scan = new Scanner(System.in); 
  
     int a = scan.nextInt(); 
     int b = scan.nextInt(); 
     int c = 1; 
  
- 
- 
     for(int i=1;i<=(b); ++i){ 
- 
         c = c*a; 
-    } 
- 
+    }
       System.out.println(c); 
    } 
     
