9b93417b5b84136572eed7019238e0049a121349
VickSxnchxz <82923629+VickSxnchxz@users.noreply.github.com>
2025-06-02 12:12:44 -0600

    Update E2.java

--- SPVH-25P/E2.java
+++ SPVH-25P/E2.java
@@ -16,16 +16,16 @@ public class E2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in); 
         int t = scan.nextInt();
-        if(t<21 && t>=15){
+        if(t<21 && t>15){
             System.out.println("fria!");
         }
-        else if(t<21 && t<15){
+        else if(t<=15){
             System.out.println("fria!\nRIP escamitas :(");
         }
-        else if(t>21 && t<29){
+        else if(t>=21 && t<=29){
             System.out.println("tibia");
         }
-        else if(t>29 && t<34){
+        else if(t>29 && t<=34){
             System.out.println("caliente!");
         }
         else if(t>34){
