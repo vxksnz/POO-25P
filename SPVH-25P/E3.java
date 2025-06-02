3b8694ddff8b4d29787f6005870557141e04104e
VickSxnchxz <82923629+VickSxnchxz@users.noreply.github.com>
2025-06-02 12:46:59 -0600

    Update and rename Jmain.java to E3.java

--- SPVH-25P/E3.java
+++ SPVH-25P/E3.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package jmain;
 
-/**
- *
- * @author Alumno 07
- */
+package e3;
+
+
 
 import java.util.Scanner;
-public class Jmain {
+public class E3 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
@@ -20,8 +13,6 @@ public class Jmain {
         n = scan.nextInt();
         
         int[] num = new int[100];
-        
-        
         for(int i=0; i<n;i++){
             num[i]= scan.nextInt();
         }
