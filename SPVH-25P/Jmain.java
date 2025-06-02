2e814705085068e4b3d6eff8a1f3f249fd445f6e
VickSxnchxz <82923629+VickSxnchxz@users.noreply.github.com>
2025-06-02 12:43:05 -0600

    Add files via upload

---
+++ SPVH-25P/Jmain.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jmain;
+
+/**
+ *
+ * @author Alumno 07
+ */
+
+import java.util.Scanner;
+public class Jmain {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int n,k;
+        
+        n = scan.nextInt();
+        
+        int[] num = new int[100];
+        
+        
+        for(int i=0; i<n;i++){
+            num[i]= scan.nextInt();
+        }
+        
+       k = scan.nextInt();
+       
+       for(int i=0; i<n;i++){
+           if((num[i]%k) == 0){
+                System.out.println("X");
+           }else{
+               System.out.println(num[i]);
+           }
+        }
+        
+        
+        
+    }
+    
+}
