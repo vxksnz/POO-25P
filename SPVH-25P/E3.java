2d43e13e481484e59bd190f8a780c746777dbd39
VickSxnchxz <82923629+VickSxnchxz@users.noreply.github.com>
2025-06-03 09:16:45 -0600

    Update E3.java

--- SPVH-25P/E3.java
+++ SPVH-25P/E3.java
@@ -1,34 +1,24 @@
-
-package e3;
-
-
-
 import java.util.Scanner;
-public class E3 {
+
+public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
         int n,k;
         
         n = scan.nextInt();
-        
         int[] num = new int[100];
         for(int i=0; i<n;i++){
             num[i]= scan.nextInt();
         }
-        
-       k = scan.nextInt();
-       
+        k = scan.nextInt();
        for(int i=0; i<n;i++){
-           if((num[i]%k) == 0){
-                System.out.println("X");
+           if((num[i]%k) != 0){
+                System.out.print("X");
            }else{
-               System.out.println(num[i]);
+               System.out.print(num[i]);
            }
-        }
-        
-        
-        
-    }
-    
+            System.out.print(" ");
+        } 
+    }  
 }
