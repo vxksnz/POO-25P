5ca67ec511cc893b1d111b6f2e623fa79affaf19
Víctor Sanchez <82923629+vxksnz@users.noreply.github.com>
2025-06-25 12:45:25 -0600

    Add files via upload

---
+++ SPVH_POO-25P/OrientadaObjetos/Rectangulo/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package paquete1;
+
+/**
+ *
+ * @author Alumno25
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Rectangulo rectangulo1= new Rectangulo();
+        Rectangulo rectangulo2= new Rectangulo(30,10);
+        
+        
+        System.out.println(rectangulo1.toString()); 
+        
+    }
+}
