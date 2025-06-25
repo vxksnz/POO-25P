9fc9e8b609d2ce97f33e0ddd520003a6ec5dc910
Víctor Sanchez <82923629+vxksnz@users.noreply.github.com>
2025-06-25 12:43:40 -0600

    Add files via upload

---
+++ SPVH_POO-25P/Ejercicios/E2.java
@@ -0,0 +1,34 @@
+
+/* 
+Escamitas
+
+Los expertos en peces recomiendan
+una temperatura tibia entre 21 y 29 grados (el agua está fría a menor temperatura y caliente
+a mayor temperatura), pero los peces podrán sobrevivir incluso a temperaturas no menores
+que 15 grados y no mayores que 34 grados.
+
+*/
+
+import java.util.Scanner; 
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in); 
+        int t = scan.nextInt();
+        if(t<21 && t>15){
+            System.out.println("fria!");
+        }
+        else if(t<=15){
+            System.out.println("fria!\nRIP escamitas :(");
+        }
+        else if(t>=21 && t<=29){
+            System.out.println("tibia");
+        }
+        else if(t>29 && t<=34){
+            System.out.println("caliente!");
+        }
+        else if(t>34){
+            System.out.println("caliente!\nRIP escamitas :(");
+        }
+    }    
+}
